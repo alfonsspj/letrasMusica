97bc01182540efb0a3f7cb83df2dc60356bd7f12
alfons <alfonsspj@gmail.com>
2022-09-04 02:57:10 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AppLetras from "./components/AppLetras"
+
+function App() {
+
+  return (
+
+      <AppLetras />
+
+  )
+}
+
+export default App
---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,16 @@
+import Formulario from "./Formulario"
+
+const AppLetras = () => {
+  return (
+    <>
+        <header>Búsqueda de Letras de Canciones</header>
+
+        <Formulario />
+
+        <main>            
+        </main>
+    </>
+  )
+}
+
+export default AppLetras
